0a5216597788ec1ebeb72e7d94ed9dd0a70709bc
Sagyam Thapa <sagyamthapa32@gmail.com>
2022-04-28 15:09:26 +0545

    CRUD Implemented

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,14 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModels");
 
 //@desc Get all goals
 //@route GET /api/goals
 //@access Public
 const getGoals = asyncHandler(async (req, res) => {
+	const goals = await Goal.find();
 	res.status(200).json({
-		mesaage: "Get Goals",
+		success: true,
+		goals,
 	});
 });
 
@@ -13,18 +16,44 @@ const getGoals = asyncHandler(async (req, res) => {
 //@route PUT /api/goals/:id
 //@access Public
 const updateGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({
-		message: `Update Goal ${req.params.id}`,
-	});
+	const goal = await Goal.findById(req.params.id);
+
+	if (!goal) {
+		res.status(400).json({
+			success: false,
+			message: "Goal not found",
+		});
+		throw new Error("Goal not found");
+	} else {
+		const updateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+		});
+		res.status(200).json({
+			success: true,
+			goal: updateGoal,
+		});
+	}
 });
 
 //@desc Delete a goal
 //@route DELETE /api/goals/:id
 //@access Public
 const deleteGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({
-		message: `Delete Goal ${req.params.id}`,
-	});
+	const goal = await Goal.findById(req.params.id);
+
+	if (!goal) {
+		res.status(400).json({
+			success: false,
+			message: "Goal not found",
+		});
+		throw new Error("Goal not found");
+	} else {
+		await goal.remove();
+		res.status(200).json({
+			success: true,
+			message: `Delete Goal ${req.params.id}`,
+		});
+	}
 });
 
 //@desc Add a goal
@@ -34,9 +63,12 @@ const addGoal = asyncHandler(async (req, res) => {
 	if (!req.body.text) {
 		throw new Error("No text provided");
 	}
-	console.log(req.body.text);
+	const goal = await Goal.create({
+		text: req.body.text,
+	});
 	res.status(200).json({
-		message: "Add Goal",
+		success: true,
+		goal,
 	});
 });
 
